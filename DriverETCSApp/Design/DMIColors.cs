76f018294d2cbe4f3af2146740a1ec3aa9cd1329
Bartosz Pyzio <b02@op.pl>
2024-10-11 19:20:41 +0200

    Added predefined colors

---
+++ DriverETCSApp/Design/DMIColors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DriverETCSApp.Design
+{
+    public static class DMIColors
+    {
+        public static Color White = Color.FromArgb(255, 255, 255);
+        public static Color Black = Color.FromArgb(0, 0, 0);
+        public static Color Grey = Color.FromArgb(195, 195, 195);
+        public static Color MediumGrey = Color.FromArgb(150, 150, 150);
+        public static Color DarkGrey = Color.FromArgb(85, 85, 85);
+        public static Color DarkBlue = Color.FromArgb(3, 17, 34);
+        public static Color Shadow = Color.FromArgb(8, 24, 57);
+        public static Color Yellow = Color.FromArgb(223, 233, 0);
+        public static Color Orange = Color.FromArgb(234, 145, 0);
+        public static Color Red = Color.FromArgb(191, 0, 2);
+        public static Color PASPDark = Color.FromArgb(33, 49, 75);
+        public static Color PASPLight = Color.FromArgb(41, 74, 107);
+    }
+}
