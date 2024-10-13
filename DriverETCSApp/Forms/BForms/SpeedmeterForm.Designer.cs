f8e5f291575b8e115c3a87f05cfec8207948459f
Alex <266558@student.pwr.edu.pl>
2024-10-13 19:33:19 +0200

    Added clock and needle. needle is controlled by SetSpeed Function of SpeedmeterForm class

--- DriverETCSApp/Forms/BForms/SpeedmeterForm.Designer.cs
+++ DriverETCSApp/Forms/BForms/SpeedmeterForm.Designer.cs
@@ -23,45 +23,44 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.clockPanel = new System.Windows.Forms.Panel();
+            this.btnTest1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // clockPanel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "test1";
+            this.clockPanel.BackColor = System.Drawing.Color.Transparent;
+            this.clockPanel.Location = new System.Drawing.Point(22, 12);
+            this.clockPanel.Name = "clockPanel";
+            this.clockPanel.Size = new System.Drawing.Size(500, 500);
+            this.clockPanel.TabIndex = 2;
+            this.clockPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.clockPanel_Paint);
             // 
-            // label2
+            // btnTest1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(502, 539);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "test2";
+            this.btnTest1.Location = new System.Drawing.Point(429, 526);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(75, 23);
+            this.btnTest1.TabIndex = 3;
+            this.btnTest1.Text = "speed up";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
             // 
             // SpeedmeterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 561);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest1);
+            this.Controls.Add(this.clockPanel);
             this.Name = "SpeedmeterForm";
             this.Text = "SpeedmeterForm";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel clockPanel;
+        private System.Windows.Forms.Button btnTest1;
     }
 }
--- DriverETCSApp/Forms/BForms/SpeedmeterForm.cs
+++ DriverETCSApp/Forms/BForms/SpeedmeterForm.cs
@@ -7,11 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DriverETCSApp.Forms.BForms {
     public partial class SpeedmeterForm : BorderLessForm {
+
+        private const int linesCount = 18;
+        private const int linesLength = 30;
+        private const int speedPerLine = 10;
+        private const int clockAngle = 270;
+        private const int clockAngleOffset = -135;
+        private const int needleCircleRadius = 30;
+
+        private int clockSize;
+        private int halfClockSize;
+        private int needleLength;
+
+        private int speed = 0;
+
         public SpeedmeterForm() {
             InitializeComponent();
+            this.DoubleBuffered = true;
+
+            clockSize = (int)(clockPanel.Width * 0.99f);
+            halfClockSize = (int)(clockSize / 2f);
+
+            needleLength = halfClockSize - 50;
         }
+
+        private void clockPanel_Paint(object sender, PaintEventArgs e) {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            
+
+            // Draw the dial background
+            g.FillEllipse(Brushes.White, 0, 0, clockSize, clockSize);
+            g.DrawEllipse(Pens.Black, 0, 0, clockSize, clockSize);
+
+            // Draw the ticks and numbers            
+            for (int i = 0; i <= linesCount; i++) {
+                int angle = i * clockAngle / linesCount - clockAngleOffset;
+                double radians = angle * Math.PI / 180;
+                int x1 = halfClockSize + (int)(halfClockSize * Math.Cos(radians));
+                int y1 = halfClockSize + (int)(halfClockSize * Math.Sin(radians));
+
+                int x2 = halfClockSize + (int)((halfClockSize - linesLength / (i % 2 + 1)) * Math.Cos(radians));
+                int y2 = halfClockSize + (int)((halfClockSize - linesLength / (i % 2 + 1)) * Math.Sin(radians));
+                g.DrawLine(Pens.Black, x1, y1, x2, y2);
+
+                // Draw speed numbers
+                if (i % 2 == 0) {
+                    string text = (i * speedPerLine).ToString();
+                    int xText = halfClockSize + (int)((halfClockSize - linesLength - 10) * Math.Cos(radians)) - 10;
+                    int yText = halfClockSize + (int)((halfClockSize - linesLength - 10) * Math.Sin(radians)) - 10;
+                    g.DrawString(text, this.Font, Brushes.Black, xText, yText);
+                }
+            }
+
+            float needleTarget = speed / (float)speedPerLine;
+
+            // Draw the needle
+            int needleAngle = (int)(needleTarget * clockAngle / linesCount) - clockAngleOffset;
+            double needleRadians = needleAngle * Math.PI / 180;
+            int xNeedle = halfClockSize + (int)(needleLength * Math.Cos(needleRadians));
+            int yNeedle = halfClockSize + (int)(needleLength * Math.Sin(needleRadians));
+            g.DrawLine(new Pen(Color.Red, 10), halfClockSize, halfClockSize, xNeedle, yNeedle);
+
+            // Draw needle circle
+            g.FillEllipse(Brushes.Red, halfClockSize - needleCircleRadius, halfClockSize - needleCircleRadius, needleCircleRadius * 2, needleCircleRadius * 2);
+            { 
+                Font largerFont = new Font(this.Font.FontFamily, 20f, this.Font.Style, this.Font.Unit);
+                string text = speed.ToString();
+                SizeF textSize = e.Graphics.MeasureString(text, largerFont);
+                int begin = (halfClockSize - needleCircleRadius);
+
+                int xText = begin + (needleCircleRadius * 2 - (int)textSize.Width) / 2;
+                int yText = begin + (needleCircleRadius * 2 - (int)textSize.Height) / 2;
+
+                g.DrawString(speed.ToString(), largerFont, Brushes.Black, xText, yText);
+            }
+        }
+
+        private void btnTest1_Click(object sender, EventArgs e) {
+            SetSpeed(this.GetSpeed() + 5);
+        }
+
+        public int GetSpeed() {
+            return speed;
+        }
+
+        public void SetSpeed(int newSpeed) {
+            speed = newSpeed;
+            clockPanel.Invalidate();
+        }
+        
     }
 }
