24d9b924d6ecd40de729f63d75bb3364857e7afb
Bartosz Pyzio <b02@op.pl>
2024-10-13 17:19:14 +0200

    removed layout syspend and resume

--- DriverETCSApp/Forms/MainForm.cs
+++ DriverETCSApp/Forms/MainForm.cs
@@ -45,29 +45,24 @@ namespace DriverETCSApp.Forms
 
         public void HideGFPanels()
         {
-            MainPanel.SuspendLayout();
             gPanel.Visible = false;
             fPanel.Visible = false;
             gForm?.Close();
             fForm?.Close();
             dPanel.Width = 612;
             dPanel.Height = 900;
-            MainPanel.ResumeLayout(true);
         }
 
         public void ShowGFPanels()
         {
-            MainPanel.SuspendLayout();
             gPanel.Visible = true;
             fPanel.Visible = true;
             dPanel.Width = 492;
             dPanel.Height = 600;
-            MainPanel.ResumeLayout(true);
         }
 
         public void SetFullScreen()
         {
-            MainPanel.SuspendLayout();
             aPanel.Visible = false;
             bPanel.Visible = false;
             cPanel.Visible = false;
@@ -79,12 +74,10 @@ namespace DriverETCSApp.Forms
             dPanel.Width = 1280;
             dPanel.Height = 900;
             dPanel.Location = new Point(0, 30);
-            MainPanel.ResumeLayout(true);
         }
 
         public void HideFullScreen()
         {
-            MainPanel.SuspendLayout();
             aPanel.Visible = true;
             bPanel.Visible = true;
             cPanel.Visible = true;
@@ -92,7 +85,6 @@ namespace DriverETCSApp.Forms
             dPanel.Width = 612;
             dPanel.Height = 900;
             dPanel.Location = new Point(668, 30);
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawYZFormDefault()
@@ -117,141 +109,115 @@ namespace DriverETCSApp.Forms
 
         public void DrawDFormIDDriver(bool isActive = true)
         {
-            MainPanel.SuspendLayout();
             HideGFPanels();
             dForm = new DForms.IDDriverForm(this, isActive);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawDFormETCSLevel(bool isActive = true)
         {
-            MainPanel.SuspendLayout();
             HideGFPanels();
             dForm = new DForms.ETCSLevelForm(this, isActive);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawDFromTrainNumer()
         {
-            MainPanel.SuspendLayout();
             HideGFPanels();
             dForm = new DForms.TrainNumberForm(this);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawDFormMenu()
         {
-            MainPanel.SuspendLayout();
             HideGFPanels();
             dForm?.Close();
             dForm = new DForms.MenuForm(this);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawTrainDataCategoryForm()
         {
-            MainPanel.SuspendLayout();
             dForm = new DForms.TrainDataTypeForm(this);
             dForm.TopLevel = false;
             SetFullScreen();
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawTrainDataInput()
         {
-            MainPanel.SuspendLayout();
             dForm = new DForms.TrainDataForm(this);
             dForm.TopLevel = false;
             SetFullScreen();
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawTrainDataTypeConfirm(PredefinedTrain trainData)
         {
-            MainPanel.SuspendLayout();
             dForm = new DForms.CheckTrainTypeDataForm(this, trainData);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawTrainDataConfirm(string trainCat, string length, string vmax, string brakingMass)
         {
-            MainPanel.SuspendLayout();
             dForm = new DForms.CheckTrainDataForm(this, trainCat, length, vmax, brakingMass);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawSettings(bool isFromDriverID, bool driverIDActiveFlag = true)
         {
-            MainPanel.SuspendLayout();
             dForm?.Close();
             dForm = new DForms.SettingsForm(this, isFromDriverID, driverIDActiveFlag);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawMainDForm()
         {
-            MainPanel.SuspendLayout();
             dForm = new DForms.MainDForm(this);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawTrainDataInfoForm()
         {
-            MainPanel.SuspendLayout();
             dForm?.Close();
             dForm = new DForms.InfoTrainDataForm(this);
             dForm.TopLevel = false;
             dPanel.Controls.Add(dForm);
             dForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawGForm()
         {
-            MainPanel.SuspendLayout();
             gForm = new GForms.ClockForm();
             gForm.TopLevel = false;
             gPanel.Controls.Add(gForm);
             gForm.Show();
-            MainPanel.ResumeLayout(true);
         }
 
         public void DrawFForm()
         {
-            MainPanel.SuspendLayout();
             fForm = new FForms.ToolbarForm(this);
             fForm.TopLevel = false;
             fPanel.Controls.Add(fForm);
             fForm.Show();
-            MainPanel.ResumeLayout(true);
         }
     }
 }
