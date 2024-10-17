b028df09abc9571b090ae1584871d149edfccc9b
Bartosz Pyzio <b02@op.pl>
2024-10-17 15:33:27 +0200

    Small fix of drawing speed chart

--- DriverETCSApp/Forms/DForms/MainDForm.cs
+++ DriverETCSApp/Forms/DForms/MainDForm.cs
@@ -69,18 +69,24 @@ namespace DriverETCSApp.Forms.DForms
         {
             TrainSpeedsAndDistances.Speeds = new List<double> { 40, 20, 40, 10, 20, 10, 20, 30, 40, 20, 0 };
             TrainSpeedsAndDistances.SpeedDistances = new List<double> { 0, 150, 500, 800, 1000, 1550, 2000, 2540, 3500, 5810, 7000 };
+            TrainSpeedsAndDistances.Gradients = new List<int> { 0 };
+            TrainSpeedsAndDistances.GradientsDistances = new List<double> { 0, 7000 };
             SpeedSegragation.CalculateSpeeds();
             this.Invalidate();
             await Task.Delay(3000);
 
             TrainSpeedsAndDistances.Speeds = new List<double> { 40,  0 };
             TrainSpeedsAndDistances.SpeedDistances = new List<double> { 0, 500 };
+            TrainSpeedsAndDistances.Gradients = new List<int> { 40, 20, 40, -10, 20, 10, 20, -30, 40, 20 };
+            TrainSpeedsAndDistances.GradientsDistances = new List<double> { 0, 150, 500, 800, 1000, 1550, 2000, 2540, 3500, 5810, 7000 };
             SpeedSegragation.CalculateSpeeds();
             this.Invalidate();
             await Task.Delay(3000);
 
             TrainSpeedsAndDistances.Speeds = new List<double> { 160, 140, 120, 200, 60, 0 };
             TrainSpeedsAndDistances.SpeedDistances = new List<double> { 0, 500, 750, 800, 1640, 2050 };
+            TrainSpeedsAndDistances.Gradients = new List<int> { 0, 10, 20 };
+            TrainSpeedsAndDistances.GradientsDistances = new List<double> { 0, 1000, 4000, 7000 };
             SpeedSegragation.CalculateSpeeds();
             this.Invalidate();
         }
