8013e3e0bf4d62ae74b04a2922e0f5b272c07a0f
Alex <266558@student.pwr.edu.pl>
2024-10-17 18:35:30 +0200

    Added Termometer for A Form

--- DriverETCSApp/Forms/AForms/PIMForm.Designer.cs
+++ DriverETCSApp/Forms/AForms/PIMForm.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.btnTest1 = new System.Windows.Forms.Button();
+            this.panelPIM = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnTest1
@@ -36,12 +37,23 @@
             this.btnTest1.UseVisualStyleBackColor = true;
             this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
             // 
+            // panelPIM
+            // 
+            this.panelPIM.BackColor = System.Drawing.Color.Transparent;
+            this.panelPIM.Location = new System.Drawing.Point(0, 0);
+            this.panelPIM.Margin = new System.Windows.Forms.Padding(4);
+            this.panelPIM.Name = "panelPIM";
+            this.panelPIM.Size = new System.Drawing.Size(144, 638);
+            this.panelPIM.TabIndex = 3;
+            this.panelPIM.Paint += new System.Windows.Forms.PaintEventHandler(this.clockPanel_Paint);
+            // 
             // PIMForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(17)))), ((int)(((byte)(34)))));
             this.ClientSize = new System.Drawing.Size(144, 738);
+            this.Controls.Add(this.panelPIM);
             this.Controls.Add(this.btnTest1);
             this.Name = "PIMForm";
             this.Text = "PIMForm";
@@ -52,5 +64,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnTest1;
+        private System.Windows.Forms.Panel panelPIM;
     }
 }
--- DriverETCSApp/Forms/AForms/PIMForm.cs
+++ DriverETCSApp/Forms/AForms/PIMForm.cs
@@ -10,8 +10,56 @@ using System.Windows.Forms;
 
 namespace DriverETCSApp.Forms.AForms {
     public partial class PIMForm : BorderLessForm {
+
+        float rectStartX = 0.6f; // Percentage 0-1
+        float rectStartY = 0.2f;
+        float rectWidth = 0.3f;
+        float rectHeight = 0.8f;
+
+        List<(float percentage, bool isBold)> listOfLines = new List<(float, bool)> { 
+            (1f, true),
+            (0.5f, true),
+            (0.25f, false),
+            (0f, true)
+        }; 
+
+        int distanceLeft = 0;
+        int columnHeight = 100; // percent 0-100
+
         public PIMForm() {
             InitializeComponent();
+
+        }
+
+        private void clockPanel_Paint(object sender, PaintEventArgs e) {
+            var g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            using(Brush brush = Brushes.White) {
+                int x = (int)(panelPIM.Width * rectStartX);
+                int y = (int)(panelPIM.Height * rectStartY);
+                int width = (int)(panelPIM.Width * rectWidth);
+                int height = (int)(panelPIM.Height * rectHeight);
+                Rectangle rect = new Rectangle(x, y, width, height);
+
+                g.FillRectangle(brush, rect);
+            }
+
+            //using (Brush brush = Brushes.White) {
+            //    string text = "100";
+            //    int xText = 0;
+            //    int yText = 0;
+            //    g.DrawString(text, numbersFont, brush, xText, yText);
+            //}
+
+            foreach ((float percentage, bool isBold) in listOfLines) {
+                int x1 = 1;
+                int x2 = (int)(panelPIM.Width * rectStartX * 0.9f);
+                int y = (int)(panelPIM.Height * rectStartY) + (int)(panelPIM.Height * rectHeight * percentage);
+                Pen pen = new Pen(Color.White, isBold ? 5f : 2f);
+
+                g.DrawLine(pen, x1, y, x2, y);
+            }
         }
 
         private void btnTest1_Click(object sender, EventArgs e) {
